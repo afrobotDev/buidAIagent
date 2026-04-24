ed61dc594edb384f1b56efc08dada384fb45d502
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-24 19:59:50 +0300

    feat: toggle verbose on for a lot more context into the console

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
     # argparse 
     parser = argparse.ArgumentParser(description="Gemini-powered Chatbot")
     parser.add_argument("user_prompt", type=str, help="Prompt to the Chatbot")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -44,9 +45,10 @@ def main():
         raise RuntimeError('Response missing usage_metadata..')
 
     else:
-
-        print(f'Prompt tokens: {usage.prompt_token_count}')
-        print(f'Response tokens: {usage.candidates_token_count}')
+        if args.verbose:
+            print(f'User prompt: {args.user_prompt}')
+            print(f'Prompt tokens: {usage.prompt_token_count}')
+            print(f'Response tokens: {usage.candidates_token_count}')
 
 
     print(response.text)
