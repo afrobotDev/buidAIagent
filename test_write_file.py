bb821cddc736cffae2800f41c6daf55956d23175
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-29 16:08:27 +0300

    Add call for write_file with different arguments

--- test_write_file.py
+++ test_write_file.py
@@ -1,1 +1,18 @@
-*
+from functions.write_file import write_file 
+
+filepath = "lorem.txt"
+print(f"Result for '{filepath}' file:")
+print(write_file("calculator", filepath, "wait, this isn't lorem ipsum"))
+print("-----------------------------------")
+
+filepath = "pkg/morelorem.txt"
+print(f"Result for '{filepath}' file:")
+print(write_file("calculator", filepath, "lorem ipsum dolor sit amet"))
+print("-----------------------------------")
+
+filepath = "/tmp/temp.txt"
+print(f"Result for '{filepath}' file:")
+print(write_file("calculator", filepath, "this should not be allowed"))
+print("-----------------------------------")
+
+
