db229c7f38fbd7488b0d27ded743923661fd483b
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-29 11:06:15 +0300

    fix: add description for output in the test file that calls get_files_info.py

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,18 +1,25 @@
 from functions.get_files_info import get_files_info 
 
+directory = "."
+if directory == '.':
+    print(f"Result for current directory:")
+    print(get_files_info("calculator", directory)) 
+    print("-----------------------------------")
 
-print(get_files_info("calculator", ".")) 
+directory = "pkg"
+print(f"Result for '{directory}' directory:")
+print(get_files_info("calculator", directory))
 
 print("-----------------------------------")
 
-print(get_files_info("calculator", "pkg"))
+directory = "/bin"
+print(f"Result for '{directory}' directory:")
+print(get_files_info("aiagent/calculator", directory))
 
 print("-----------------------------------")
 
-print(get_files_info("calculator", "/bin"))
-
-print("-----------------------------------")
-
-print(get_files_info("calculator", "../"))
+directory = "../"
+print(f"Result for '{directory}' directory:")
+print(get_files_info("aiagent/calculator", directory))
 
 
