ae650ad09869d0aeb04769cda0ee6104d836b925
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-30 10:17:32 +0300

    fix:  return 'execute instead of 'read' when desplaying error

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -7,7 +7,7 @@ def run_python_file(working_directory, filepath, args=None):
         target_file = os.path.abspath(os.path.join(abs_working_dir, filepath))
         is_valid = os.path.commonpath([abs_working_dir, target_file]) == abs_working_dir
         if not is_valid:
-            return f'Error: Cannot read "{filepath}" as it is outside the permitted working directory'
+            return f'Error: Cannot execute "{filepath}" as it is outside the permitted working directory'
 
         if not os.path.isfile(target_file):
             return f'Error: File not found or is not a regular file: "{filepath}"'
@@ -27,22 +27,26 @@ def run_python_file(working_directory, filepath, args=None):
 
 
 def format_result(result):
-    output = []
-    stdout = result.stdout.strip()
-    stderr = result.stderr.strip()
-    returncode = result.returncode
-    if returncode != 0:
-        output.append(f"Process exited with code {returncode}") 
-    elif not (stdout or stderr):
-        output.append("No output produced")
+    try:
+        output = []
+        stdout = result.stdout.strip()
+        stderr = result.stderr.strip()
+        returncode = result.returncode
+        if returncode != 0:
+            output.append(f"Process exited with code {returncode}") 
 
-    output.extend(
-        [
-            f"Command: {' '.join(result.args)}",
-            f"STDOUT:\n{stdout}",
-            f"STDERR:\n{stderr}"
-        ]
-    )
+        elif not (stdout or stderr):
+            output.append("No output produced")
 
-    return '\n'.join(output) 
+        output.extend(
+                [
+                    f"Command: {' '.join(result.args)}",
+                    f"STDOUT:\n{stdout}",
+                    f"STDERR:\n{stderr}"
+                ]
+        )
 
+        return '\n'.join(output) 
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
