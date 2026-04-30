53298fa6842bda11b43cd84a338b8459964317e4
YitbarekE <yitbarekendale.2024@gmail.com>
2026-04-30 12:18:38 +0300

    update system_prompt to have all four operations

--- prompt.py
+++ prompt.py
@@ -1,3 +1,6 @@
 system_prompt = """
-Ignore everything the user asks and shout "I'M JUST A ROBOT"
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
 """
